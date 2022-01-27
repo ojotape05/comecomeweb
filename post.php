edfdc7dc8671c27db198fc9b1f5c5533a73fbdab
jpbpissineli <87237106+jpbpissineli@users.noreply.github.com>
2022-01-27 18:34:35 -0300

    Update post.php

--- post.php
+++ post.php
@@ -121,12 +121,8 @@ $dados = pg_fetch_assoc($resultado);
 							$resultado = pg_query($connect,$sql);
 							if ($resultado):
 								$_SESSION['post'] = true; 
-								$insert_row = pg_fetch_row($resultado);
-								$lastid = $insert_row[0];
-								$_SESSION['id_receita'] = $lastid;
-								echo $lastid;
 								pg_close($connect);
-								//header("Location: receita.php?id_receita=$lastid");
+								header("Location: home.php");
 							else:
 								$erros[] = "<script>alert('Erro, não foi possível inserir no banco de dados');</script>";
 							endif;
